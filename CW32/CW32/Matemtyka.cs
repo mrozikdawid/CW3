1ee915e972156726100111eb41e75b81e9bb9eda
mrozikdawid <mrozik.dawid@gmail.com>
2017-10-26 12:17:11 +0200

    Add files via upload

---
+++ CW32/CW32/Matemtyka.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW32
+{
+    class Matematyka
+    {
+        private static double pi = 3.14; // prywatne statyczne pole pi=3.14
+
+        public static double  PoleKola(int r) //publiczna statyczna metoda PoleKola z parametrm r
+        {
+            return pi * r * r;
+        }
+
+        public static double ObwodKola(int r) //publiczna statyczna metoda ObwodKola z parametrem r
+        {
+            return 2 * Matematyka.pi * 4;
+        }
+    }
+}
