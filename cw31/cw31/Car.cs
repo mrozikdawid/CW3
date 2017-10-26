d7f2495fabf0e9093941e7eace6cd57536923678
mrozikdawid <mrozik.dawid@gmail.com>
2017-10-26 12:05:29 +0200

    Add files via upload

---
+++ cw31/cw31/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw31
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+
+        private Car() //konstruktor bez parametrów
+        {
+
+        }
+
+        private Car(double pojemnoscSilnika, string marka) //przeciążenie konstruktora poprzez stworzenie nowego konstruktora parametrycznego
+        {
+            this.pojemnoscSilnika = pojemnoscSilnika; //ustawienie wartości pola
+            this.marka = marka; // ustawienie wartości pola
+        }
+
+        public void dodajmarke(string marka) // tworzenie obiektu za pomocą konstruktora parametrycznego
+        {
+            this.marka = marka;
+            Console.WriteLine("marka samochody {0}",marka);
+        }
+
+        public static Car Create(double pojemnosc, string marka) //Metoda która zwraca obiekt 
+        {
+            Car temp = new Car(pojemnosc, marka);
+            return temp;
+        }
+
+        public readonly static int iloscKol; // statyczne pole tylko do odczytu
+
+        static Car() // konstruktor tworzący iloscKol
+        {
+            iloscKol = 4;
+        }
+        public const int iloscDrzwi = 4; // pupliczne stale pole iloscDrzwi
+
+        ~Car() //destruktor 
+        {
+            Console.WriteLine("Zwalniam pamiec");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cw31/cw31/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car car1 = new Car(); tworznie obiektu
+            //car1.pojemnoscSilnika = 1999.9; dodawanie wartości        
+            //car1.marka = "Ford"; dodawanie wartości do obiektu
+
+            //Car car2 = new Car(); tworznie obiektu 
+            //car2.dodajmarke("mercedes"); dodawanie wartości do obiektu z pomocą konstruktora
+
+            Car car3 = Car.Create(233,"Skoda"); //stworzenie obiektu poprzez metode create
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
