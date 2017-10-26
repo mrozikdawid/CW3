d5ba17a8c3254b25966c44aafef19361df36b41d
mrozikdawid <mrozik.dawid@gmail.com>
2017-10-26 12:47:34 +0200

    Add files via upload

---
+++ CW34/CW34/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samochód samochód1 = new Samochód();
+            samochód1.Model = "Fabia";
+            samochód1.Marka = "Skoda";
+
+            samochód1.Cena = 5000.24;
+
+
+            //Console.WriteLine(Double.Parse(samochód1.Cena));
+        }        
+    }
+}
+
---
+++ CW34/CW34/Samochód.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW34
+{
+    class Samochód
+    {
+        private string model;
+
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        private double cena;
+
+        public double Cena 
+        {
+            get { return cena; }
+            set { cena = value; }
+        }
+
+        private string marka;
+
+        public string Marka
+        {
+            get { return marka; }
+            set { marka = value; }
+        }
+
+    }
+}
